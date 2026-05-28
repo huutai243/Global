636a49abeffaf25d16944685ec7b088e013e85fe
huutai2403 <huutai2403@gmail.com>
2026-05-28 22:23:53 +0700

    update code

--- ECommerce.WebApi/Infras/Extensions/RepositoryExtensions.cs
+++ ECommerce.WebApi/Infras/Extensions/RepositoryExtensions.cs
@@ -1,9 +1,16 @@
+using ECommerce.Core.SharedLibs.Helpers;
+using ECommerce.Infrastructure.Storage;
+using ECommerce.Core.SharedLibs.Interfaces;
+
 namespace ECommerce.WebApi.Infras.Extensions;
 
 public static class RepositoryExtensions
 {
     public static IServiceCollection AddRepositories(this IServiceCollection services, IConfiguration configuration)
     {
+        services.AddSingleton<IJsonHelper, JsonHelper>();
+        services.AddScoped<IBlobStorageService, AzureBlobFileStorageService>();
+
         return services;
     }
 }
